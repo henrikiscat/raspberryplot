3f284f627070b6082d16a6c75573f22225f79d1c
henrikiscat <henrikis@protonmail.com>
2021-06-18 15:38:17 +0200

    Add second plot with test data.

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import matplotlib.pyplot as plt
 import ast
 import pandas as pd
+from matplotlib.widgets import CheckButtons
+import matplotlib.colors as clrs
+
+
+def my_plotter(ax, data, param_dict):
+    return ax.plot(data, **param_dict)
 
 
 with open('logfile9_12_7.log', 'r') as f:
@@ -17,18 +23,52 @@ with open('logfile9_12_7.log', 'r') as f:
         dt_new = total_time[j + 1] - total_time[j]
         if dt_new < dt:
             dt = dt_new
-            print("Timestamp: {}, deltatime: {}".format(total_time[j], dt))
     for var in messages[0:7]:
         columns_ += list(var.keys())
-
     df = pd.DataFrame(index=total_time, columns=columns_)
+    df_pec = pd.read_csv('Test15448_head_removed.csv', index_col=11)
     for j in range(len(messages)):
         for key in messages[j]:
             df.at[total_time[j], str(key)] = messages[j][key]
-    plt.plot(df.index, df.values, '-+')
-    plt.legend(columns_, loc='upper left', bbox_to_anchor=(1, 1))
-    plt.tight_layout()
+
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(18, 8))
+    ax1.set_title('Raspberry log, TestID: 15488')
+    ax2.set_title('PEC log, TestID: 15488')
+    ax2.set_xlabel('Total time [s]')
+    ax1.set_ylabel('mV')
+    ax2.set_ylabel('mV')
+
+    plt.subplots_adjust(left=0.26)
+    df_pec = df_pec.iloc[:, 24:(24 + 30)]
+    df = df[df.index > 105]
+    #print(({x:x.split()[0] for x in df_pec.columns}))
+    df_pec.rename(columns = {x:x.split()[0] for x in df_pec.columns},inplace=True)
+    print(df_pec.columns)
+    lines = [ax1.plot(df[x].index, df[x].values, label=x, marker='.', markersize=5) for x in df.columns]
+    lines_pec = [ax2.plot(df_pec[x].index, df_pec[x].values, label=x, marker='.', markersize=5) for x in df_pec.columns]
+    colors = [clrs.to_rgba(x.get_color()) for x in ax1.get_lines()]
+    box = plt.Rectangle(width=0.1, height=0.1, xy=(0, 1))
+    rax = plt.axes([0.02, 0.1, 0.19, 0.7])
+    labels = df.columns.values.tolist()
+    labels_pec = df_pec.columns.values.tolist()
+    print("rasp: {}, pec: {}".format(labels, labels_pec))
+    visibility = [line[0].get_visible() for line in lines]
+    check = CheckButtons(rax, labels, visibility)
+    i = 0
+    for label in check.labels:
+        label.set_color(colors[i])
+        i += 1
+
+    def func(label):
+        index = labels.index(label)
+        index_pec = labels_pec.index(label)
+        lines[index][0].set_visible(not lines[index][0].get_visible())
+        lines_pec[index_pec][0].set_visible(not lines_pec[index_pec][0].get_visible())
+        plt.draw()
+
+    check.on_clicked(func)
     plt.show()
+
 
 
 def write_csv():
@@ -56,5 +96,4 @@ def write_csv():
 
 
 #write_csv()
-
 
